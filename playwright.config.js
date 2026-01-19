8cfabf51a9835f9df14f5468eab178a3119424f9
AkshayG <goelakshay001@gmail.com>
2026-01-19 15:09:43 +0530

    Config options

--- playwright.config.js
+++ playwright.config.js
@@ -25,16 +25,44 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    browserName : 'chromium',
-    headless : false,
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    screenshot: 'on',
-  },
+  projects: [
+
+    {
+      name: 'chrome',
+      use: {
+        browserName: 'chromium',
+        headless: false,
+        /* Base URL to use in actions like `await page.goto('')`. */
+        // baseURL: 'http://localhost:3000',
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'retain-on-failure',
+        screenshot: 'on',
+        video : 'retain-on-failure',
+        ignoreHTTPSErrors: true,
+        permissions: ['geolocation'],
+        viewport : { width: 720, height: 750 },
+        //...devices['Galaxy S III'],
+      }
+    },
+     {
+      name: 'firefox',
+      use: {
+        browserName: 'firefox',
+        headless: true,
+        /* Base URL to use in actions like `await page.goto('')`. */
+        // baseURL: 'http://localhost:3000',
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'retain-on-failure',
+        screenshot: 'on',
+      }
+    }
+
+  ]
+
+
 
   /* Configure projects for major browsers */
   // projects: [
@@ -53,25 +81,25 @@ export default defineConfig({
   //     use: { ...devices['Desktop Safari'] },
   //   },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+  /* Test against mobile viewports. */
+  // {
+  //   name: 'Mobile Chrome',
+  //   use: { ...devices['Pixel 5'] },
+  // },
+  // {
+  //   name: 'Mobile Safari',
+  //   use: { ...devices['iPhone 12'] },
+  // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+  /* Test against branded browsers. */
+  // {
+  //   name: 'Microsoft Edge',
+  //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+  // },
+  // {
+  //   name: 'Google Chrome',
+  //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+  // },
   //],
 
   /* Run your local dev server before starting the tests */
